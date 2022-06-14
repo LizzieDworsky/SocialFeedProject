64a202697af222b5e0b77403be992068600b4445
LizzieDworsky <amydworsky15@gmail.com>
2022-06-14 15:02:16 -0600

    wrote the logic for PostMapper.jsx and Post.jsx and connected them so they display on app.jsx

--- social-feed/src/App.jsx
+++ social-feed/src/App.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import CreatePost from "./Components/CreatePost";
 import NavBar from "./Components/NavBar";
 import PostMapper from "./Components/PostMapper";
 
 function App() {
+    //Stateful variables
+    const [posts, setPosts] = useState([
+        {
+            id: 1,
+            name: "Jake",
+            message: "I like pizza!",
+            isLiked: false,
+            isDisliked: false,
+        },
+        {
+            id: 2,
+            name: "Pascal",
+            message: "I like cereal!",
+            isLiked: false,
+            isDisliked: false,
+        },
+        {
+            id: 3,
+            name: "Amy",
+            message: "I like flautas!",
+            isLiked: false,
+            isDisliked: false,
+        },
+    ]);
+
     return (
         <div>
             <NavBar />
             <CreatePost />
-            <PostMapper />
+            <PostMapper array={posts} />
         </div>
     );
 }
--- social-feed/src/Components/Post.jsx
+++ social-feed/src/Components/Post.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Post = (props) => {
-    return <div></div>;
+const Post = ({ post }) => {
+    return (
+        <div>
+            <h3>{post.name}</h3>
+            <p>{post.message}</p>
+            <button>{post.isLiked}</button>
+            <button>{post.isDisliked}</button>
+        </div>
+    );
 };
 
 export default Post;
